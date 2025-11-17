e438c52a5c10f10ab39e52a7d0adb6d9b7467f95
UnrelatedString <unrelatedstring@gmail.com>
2025-11-17 12:26:24 -0500

    a vexing error and also why did i have to downgrade the package set to get my test dependencies uh ohhhh

--- src/Data/Bracket.purs
+++ src/Data/Bracket.purs
@@ -2,9 +2,14 @@
 module Data.Bracket
   ( Config
   , Round(..)
+  , Seed(..)
+  , boustrophedonUnDivMod
   ) where
 
 import Prelude
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+import Data.Newtype (class Newtype)
 import Data.Reflectable
   ( class Reflectable
   , reflectType
@@ -20,7 +25,24 @@ type Config =
 -- The first round is round 1
 newtype Round :: Int -> Type
 newtype Round outOf = Round Int
+-- is this even a thing i can do i forgot
+-- derive newtype instance forall (outOf :: Int). Eq (Round outOf)
+
+-- 0-based
+newtype Seed = Seed Int
+derive newtype instance Eq Seed
+derive instance Newtype Seed _
+derive instance Generic Seed _
+
+instance Show Seed where
+  show = genericShow
 
 -- Convert to counting backwards where finals are round 0
 roundsLeft :: forall (outOf :: Int). Reflectable outOf Int => Round outOf -> Int
 roundsLeft (Round x) = reflectType (Proxy@outOf) - x
+
+-- for determining what seeds play in a match LMAO
+boustrophedonUnDivMod :: Int -> Int -> Int -> Seed
+boustrophedonUnDivMod divisor quotient remainder
+  | quotient `mod` 2 == 0 = Seed $ quotient * divisor + remainder
+  | otherwise = Seed $ (quotient + 1) * divisor - remainder - 1
---
+++ test/Test/Main.purs
@@ -0,0 +1,30 @@
+module Test.Main where
+
+import Prelude
+
+import Data.Array (zip)
+import Data.Enum (enumFromTo)
+import Data.Tuple (Tuple(..))
+import Data.Traversable (for_)
+import Effect (Effect)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.QuickCheck (quickCheck')
+import Test.QuickCheck ((===))
+import Test.Spec.Runner.Node (runSpecAndExitProcess)
+import Test.Spec.Reporter.June.Pretty (prettyReporter)
+
+import Data.Bracket
+  ( Seed(..)
+  , boustrophedonUnDivMod
+  )
+
+main :: Effect Unit
+main = runSpecAndExitProcess [prettyReporter] do
+  bracketSuite
+
+bracketSuite :: Spec Unit
+bracketSuite = describe "Core bracket logic" do
+  it "12 seeds are paired into 1v1v1s correctly" do
+    for_ (enumFromTo 0 11 `zip` [0,1,2,3,7,6,5,4,8,9,10,11]) \(Tuple x seed) ->
+      boustrophedonUnDivMod 3 (x `div` 3) (x `mod` 3) `shouldEqual` Seed seed
